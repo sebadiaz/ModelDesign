e21281a3e5a8110095756bcdfee7f70d0f0b611d
Sebastien DIAZ <sebastien.diaz@gmail.com>
2017-05-23 22:26:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 '''
 Created on 21 mai 2017
-https://www.afpy.org/Members/cedric/mas-python/pythonhowto-2009-12-04-5314484858
+
 @author: buissondiaz
 '''
 from itertools import count
-
+import datetime
 if __name__ == '__main__':
     pass
 
@@ -16,10 +16,45 @@ url = 'https://bleutrade.com/api/v2/public/getcandles?market=MONA_BTC&period=8h&
 
 item= requests.get(url).json()
 myList=[]
+myList2=[]
 counter=0
 for price in item['result']:
     myList.append(float(price['Close']))
+    data = {}
+    data["timestamp"]= datetime.datetime.strptime(price['TimeStamp'], "%Y-%m-%d %H:%M:%S");
+    data["volume"]= price['Volume'];
+    data["high"]= price['High'];
+    data["low"]= price['Low'];
+    data["close"]= price['Close'];
+    data["open"]= price['Open'];
+    data["baseVolume"]= price['BaseVolume'];
+    
+    
+    myList2.append(data)
+    print price
     counter=counter+1
+print "eee"    
+print myList2
+
+print "eee"    
+print myList2
+import pickle
+import os.path
+loadd=[]
+if os.path.isfile("save.p") :
+    loadd = pickle.load( open( "save.p", "rb" ) )
+mergedlist = loadd + myList2
+myList2=sorted(
+    mergedlist,
+    key=lambda x: x['timestamp'], reverse=False
+)
+myList3=[]
+keys=set()
+for k in myList2:
+        if k['timestamp'] not in keys:
+            keys.add(k['timestamp'])
+            myList3.append(k)
+pickle.dump( myList3, open( "save.p", "wb" ) )
 close = numpy.random.random(100)
 close = numpy.array(myList)
 output = talib.SMA(close)
@@ -53,4 +88,6 @@ with open('eggs.csv', 'wb') as csvfile:
     spamwriter.writerow(signal)
     spamwriter.writerow(hist)
     
-    
+print myList3
+print myList3.__sizeof__()
+print myList2.__sizeof__()
